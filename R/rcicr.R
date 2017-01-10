0537c808ca5dbaeb689dd9f0eafa221f83d87c86
Ron Dotsch <rdotsch@gmail.com>
2017-01-10 08:58:01 +0100

    Moved dealing with CRAN notes to zzz.R, as is customary, and fixed final CRAN note on ‘obs’ variable.

--- R/rcicr.R
+++
@@ -1,3 +0,0 @@
-
-# Suppress checking notes for variables loaded at runtime from .RData files
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("p", "s", "base_faces", "stimuli_params", "img_size"))
